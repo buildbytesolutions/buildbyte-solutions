1e6edadec2069d686718b441fe2f6ed1a0c1eabc
buildbytesolutions <buildbytesolutions89@gmail.com>
2024-10-20 00:29:20 +0530

    Update Contact.js

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -81,6 +81,7 @@ export const Contact = () => {
 													onChange={(e) =>
 														onFormUpdate("firstName", e.target.value)
 													}
+													required
 												/>
 											</Col>
 											<Col size={12} sm={6} className="px-1">
@@ -91,6 +92,7 @@ export const Contact = () => {
 													onChange={(e) =>
 														onFormUpdate("lastName", e.target.value)
 													}
+													required
 												/>
 											</Col>
 											<Col size={12} sm={6} className="px-1">
@@ -101,6 +103,7 @@ export const Contact = () => {
 													onChange={(e) =>
 														onFormUpdate("email", e.target.value)
 													}
+													required
 												/>
 											</Col>
 											<Col size={12} sm={6} className="px-1">
@@ -111,6 +114,7 @@ export const Contact = () => {
 													onChange={(e) =>
 														onFormUpdate("phone", e.target.value)
 													}
+													required
 												/>
 											</Col>
 											<Col size={12} className="px-1">
@@ -121,6 +125,7 @@ export const Contact = () => {
 													onChange={(e) =>
 														onFormUpdate("message", e.target.value)
 													}
+													required
 												></textarea>
 												<button type="submit">
 													<span>{buttonText}</span>
